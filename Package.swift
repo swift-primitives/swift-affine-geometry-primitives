0672df598f7f774f9a13ed782b16a301043ecf45
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-04-21 18:20:19 +0200

    Consume swift-format-primitives rename (swift-formatting-primitives → swift-format-primitives)
    Updates `.package(path:)`, `.product(name:)`, and
    `import Formatting_Primitives` to `import Format_Primitives` for the
    L1 rename per swift-package skill [PKG-NAME-001]/[PKG-NAME-005].

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         .package(path: "../swift-affine-primitives"),
         .package(path: "../swift-algebra-linear-primitives"),
         .package(path: "../swift-dimension-primitives"),
-        .package(path: "../swift-formatting-primitives"),
+        .package(path: "../swift-format-primitives"),
         .package(path: "../swift-numeric-primitives"),
     ],
     targets: [
@@ -28,7 +28,7 @@ let package = Package(
                 .product(name: "Affine Primitives", package: "swift-affine-primitives"),
                 .product(name: "Algebra Linear Primitives", package: "swift-algebra-linear-primitives"),
                 .product(name: "Dimension Primitives", package: "swift-dimension-primitives"),
-                .product(name: "Formatting Primitives", package: "swift-formatting-primitives"),
+                .product(name: "Format Primitives", package: "swift-format-primitives"),
                 .product(name: "Real Primitives", package: "swift-numeric-primitives"),
             ]
         ),
