773eedd23836ba4499740c24fb307998327f0332
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-05-07 17:15:46 +0200

    Drop unused dep declaration: Format Primitives

--- Package.swift
+++ Package.swift
@@ -22,7 +22,6 @@ let package = Package(
         .package(path: "../swift-affine-primitives"),
         .package(path: "../swift-algebra-linear-primitives"),
         .package(path: "../swift-dimension-primitives"),
-        .package(path: "../swift-format-primitives"),
         .package(path: "../swift-numeric-primitives"),
         .package(path: "../swift-tagged-primitives"),
     ],
@@ -33,7 +32,6 @@ let package = Package(
                 .product(name: "Affine Primitives", package: "swift-affine-primitives"),
                 .product(name: "Algebra Linear Primitives", package: "swift-algebra-linear-primitives"),
                 .product(name: "Dimension Primitives", package: "swift-dimension-primitives"),
-                .product(name: "Format Primitives", package: "swift-format-primitives"),
                 .product(name: "Real Primitives", package: "swift-numeric-primitives"),
             ]
         ),
